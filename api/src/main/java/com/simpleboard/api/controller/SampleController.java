49876991e13d83842fac2ec1ba7844a13820e0f7
HJH413 <91816743+HJH413@users.noreply.github.com>
2024-05-17 16:27:09 +0900

    backend v0.1
    1. 샘플소스생성
    2. DB연결

---
+++ api/src/main/java/com/simpleboard/api/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.simpleboard.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping(value = "/test")
+    private String aa() {
+        return "hello World";
+    }
+}
